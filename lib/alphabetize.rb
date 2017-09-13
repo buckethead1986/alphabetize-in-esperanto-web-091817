fed3381c27127451e53030dc3466999b64deec6f
Robert Peck <rspeckpfs@gmail.com>
2017-09-13 23:28:10 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,4 @@
-def alphabetize(arr)
-  # code here
+def alphabetize(arr) #The sort_by method on ruby docs doesnt make sense how I would use it. Doing it a way tat will at least work, then checking out the solution to learn how to use sort_by in this case
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by {|string| string.chars.map{|c| alphabet.index(c)}}
 end
